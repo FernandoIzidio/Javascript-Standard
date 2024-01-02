3539920474e9a19cd3b53078618ca2400a2cc29b
FernandoIzidio <luizfernando1010200@gmail.com>
2024-01-02 17:32:43 -0300

    intro back-side

---
+++ nodeLibrary/documentation.js
@@ -0,0 +1,8 @@
+/*
+Node é um interpretador javascript, que permite a utilização do javascript no server-side/ back-end
+
+npm - Gerenciador de pacotes do node
+
+npm init -y - Gera um arquivo package.json responsavel por armazenar informações importantes sobre o projeto, suas dependências, scripts de construção, entre outras configurações. Em resumo é utilizado para extruturar e gerenciar as dependências do projeto
+
+*/
