00600df667282a4f05d596013c38c6edec94367f
FernandoIzidio <luizfernando1010200@gmail.com>
2024-01-04 17:13:47 -0300

    dessignpatterns

---
+++ designPatterns.js
@@ -0,0 +1,16 @@
+/*
+CRUD(CREATE READ UPDATE DELETE)
+rpa(robotic process automation) - python
+mvp(model view presenter)
+mvc(model view controller)
+
+
+
+Singleton é um padrão de projeto onde os classes de um módulo possuiem uma única instância/objeto.
+
+Caracteristicas do padrão de projeto singleton:
+    - constructor privado para não permitir criar mais intâncias da classe
+    - metódo publico estatico que vai permitir acesso a única instância da classe
+    - instância única
+
+*/
