de74b6a52beae33f1aa110c9ca43423369b2112d
FernandoIzidio <luizfernando1010200@gmail.com>
2024-01-04 18:39:07 -0300

    norm

--- tools/docBabel.js
+++ tools/docBabel.js
@@ -1,4 +1,7 @@
 /*
+npm install --save-dev @babel/preset-env @babel/cli @babel/core babel-loader style-loader css-loader webpack webpack-cli
+npm install core-js regenerator-runtime
+
 Babel é um transpilador/transformador de código javascript, ele transpila o código javascript moderno para um versão estavel para qualquer navegador. Ele permite que qualquer navegador utilize uma aplicação web, sem ter que fazer muitos ajustes
 
 npx babel source.js -o new.js presets="@babel/env" -w - Converte o código fonte js, em código js estavel
