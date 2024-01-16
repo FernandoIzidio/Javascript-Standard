83a218b88e6b743d3dba0de7d22abf2951231126
FernandoIzidio <luizfernando1010200@gmail.com>
2024-01-15 23:21:04 -0300

    doc update

--- backSide/documentation.js
+++ backSide/documentation.js
@@ -4,11 +4,11 @@ Node é um interpretador javascript, que permite a utilização do javascript no
 __dirname - Diretório de trabalho do main(módulo em execução)
 
 npm(node package manager) - Gerenciador de pacotes do node
-npx(node package Execute) - Usado para executar pacotes node como script. node_modules/.bin do projeto e executa a versão local do pacote.
+npx(node package Execute) - Usado para executar pacotes node locais do projeto  como script.
 
 npm run key - Roda o script que esta na chave scripts do package.json. É usado para executar um script de construção
 
-
+A chave de scripts de package.json por padrão executa todo argumento que recebe com npx
 
 npm init -y - Gera um arquivo package.json responsavel por armazenar informações importantes sobre o projeto, suas dependências, scripts de construção, entre outras configurações. Em resumo é utilizado para extruturar e gerenciar as dependências do projeto
 
@@ -16,8 +16,7 @@ npm install - instala as dependências do projeto
 
 const package = require("pkgname") - importa um módulo no node.
 
-module.exports = {} - Atributo exports, é usado para exportar um dicionários com os dados do módulo
-node package execute
+module.exports = {} - Atributo exports é utilizado para exportar dados em um módulo node
 
 dependecies - Dependências necessárias para execução da aplicação. // Ambiente de produção
 devDependecies - Dependências necessárias para desenvolvimento da aplicação. //ambiente de desenvolvimento
